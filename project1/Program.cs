20e620fe489fcd263c24028d0591ab38ea131daf
MONO\praktikant <bornajova@gmail.com>
2018-10-10 14:36:34 +0200

    Init

---
+++ project1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Available commands: Add, Remove, Display, List, <role_name>List");
+            Console.ReadLine();
+            Console.Write("Role:");
+            String Role = Console.ReadLine();
+            Console.Write("FirstName:");
+            String FirstName = Console.ReadLine();
+            Console.Write("LastName:");
+            String LastName = Console.ReadLine();
+            Console.Write("Age:");
+            String Age = Console.ReadLine();
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
